6a239fde977e5b032d950fa7cca72c0c86dec1a6
n28div <nicolaslazzari98@gmail.com>
2022-07-11 19:10:19 +0200

    resources on all queries

--- mapping/mapfiles.py
+++ mapping/mapfiles.py
@@ -12,6 +12,7 @@ parser.add_argument("output_directory", help="Output directory where content wil
 parser.add_argument('--sparqlanything', help="Path to the sparql anything jar. If none will try the current directory.", default="./sparql-anything-0.6.0.jar")
 parser.add_argument('--template-name', help="Name used in the SPARQL query for the naming template, which will be used by sparqlanything. Defaults to name.", default="name")
 parser.add_argument('--format', help="Serialization format used by SPARQLAnything. Defaults to turtle.", default="ttl")
+parser.add_argument('--outprefix', help="Prefix added to the outfile. Defaults to empty string.", default="")
 args = parser.parse_args()
 
 files = os.listdir(args.input_directory)
@@ -20,7 +21,7 @@ format = args.format
 
 for file in tqdm(files):
   filepath = os.path.join(args.input_directory, file)
-  outpath = os.path.join(args.output_directory, f"{file}.ttl")
+  outpath = os.path.join(args.output_directory, f"{args.outprefix}_{file}.ttl")
 
   with open(outpath, "w") as outfile:
     process = f"java -jar {sparqlanything_path} -q {args.query} -v {args.template_name}={filepath} -f {format} > {outpath}"
